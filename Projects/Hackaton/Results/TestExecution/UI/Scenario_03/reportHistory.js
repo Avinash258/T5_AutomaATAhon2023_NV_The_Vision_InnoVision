f35a6da55e02c051e93d58a8f1ee404e3dba43e8
Avinash258 <57386491+Avinash258@users.noreply.github.com>
2023-11-25 16:48:07 +0530

    Scenario_3

---
+++ Projects/Hackaton/Results/TestExecution/UI/Scenario_03/reportHistory.js
@@ -0,0 +1,1 @@
+var reportName="UI:Scenario_03";var dataSet=[{"PassTC":"1","FailTC":"0","ReportPath":"25-Nov-2023 16-40-17\\summary.html","ExecTC":"1","ExecutionDate":"25-Nov-2023 16-40-17","ExecTime":"00:00:20"}]
